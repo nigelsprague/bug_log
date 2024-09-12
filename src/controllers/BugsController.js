a02c2322571f2cfb38583676ad9b26e34ce95d76
Nigel Sprague <spraguenigel27@gmail.com>
2024-09-12 15:12:27 -0600

    g edits

--- src/controllers/BugsController.js
+++ src/controllers/BugsController.js
@@ -8,6 +8,9 @@ export class BugsController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createBug)
+      .get('', this.getAllBugs)
+      .get('/:bugId', this.getBugById)
+      .put('/:bugId', this.editBug)
   }
 
   async createBug(request, response, next) {
@@ -22,9 +25,33 @@ export class BugsController extends BaseController {
     }
   }
 
+  async getAllBugs(request, response, next) {
+    try {
+      const bug = await bugsService.getAllBugs()
+      response.send(bug)
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
   async getBugById(request, response, next) {
     try {
+      const bugId = request.params.bugId
+      const userInfo = request.userInfo
+      const bug = await bugsService.getBugById(bugId, userInfo.id)
+      response.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
 
+  async editBug(request, response, next) {
+    try {
+      const bugId = request.params.bugId
+      const bugData = request.body
+      const bug = await bugsService.editBug(bugId, bugData)
+      response.send(bug)
     } catch (error) {
       next(error)
     }
--- src/services/BugsService.js
+++ src/services/BugsService.js
@@ -1,6 +1,32 @@
+import { BugsController } from "../controllers/BugsController";
 import { dbContext } from "../db/DbContext";
+import { Forbidden } from "../utils/Errors";
 
 class BugsService {
+  async editBug(bugId, bugData) {
+    const bugToUpdate = await dbContext.Bugs.findById(bugId)
+    bugToUpdate.closed = bugData.closed ?? bugToUpdate.closed
+    bugToUpdate.title = bugData.title ?? bugToUpdate.title
+    bugToUpdate.description = bugData.description ?? bugToUpdate.description
+    await bugToUpdate.save()
+    return bugToUpdate
+  }
+
+
+  async getAllBugs() {
+    const bug = await dbContext.Bugs.find()
+    return bug
+  }
+
+  async getBugById(bugId, userId) {
+    const bug = await dbContext.Bugs.findById(bugId)
+    await bug.populate('creator')
+    if (bug.creatorId != userId) {
+      throw new Forbidden("You cannot access another user's bug");
+    }
+    return bug
+  }
+
   async createBug(bugData) {
     const bug = await dbContext.Bugs.create(bugData)
     await bug.populate('creator')
